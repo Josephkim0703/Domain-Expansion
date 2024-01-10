5b38da2b4d5731547003f1ae2df083cb11f776f5
Joseph <josephkim0703@gmail.com>
2024-01-10 03:25:56 -0800

    data

--- info.js
+++ info.js
@@ -25,6 +25,62 @@ function setup(){
     document.getElementById("translation").innerHTML = sorcerer[storedIndex].translation;
     document.getElementById("lore").innerHTML = sorcerer[storedIndex].lore;
     document.getElementById("hand").src = sorcerer[storedIndex].hand;
+
+    if(storedIndex == 4){
+        document.getElementById("left").style.transform = "translateX(" + 3 +"rem)";
+        document.getElementById("right").style.transform = "translateX(-" + 3 +"rem)";
+        document.getElementById("appearance").style.left = 14.5 +"rem";
+        document.getElementById("appearance").style.top = 38.8 +"rem";
+        document.getElementById("hand").style.transform = "translateX(" + 4.7 +"rem)";
+        document.getElementById("domain-more-info").style.width = 47+"rem";
+        document.getElementById("lore").style.width = 47+"rem";
+        document.getElementById("domain-info").style.width = 25+"rem";
+        document.getElementById("extra").style.width = 29+"rem";
+        document.getElementById("extra").style.transform = "translate(-" + 9 +"rem)";
+        document.getElementById("manga").style.marginRight = "-" + 10 +"rem)";
+        document.getElementById("user-info").style.width = 47+"rem";
+        document.getElementById("domain-vid").style.visibility = "hidden";
+        document.getElementById("translation").style.transform = "translate("+ 10 +"rem," + 0.6 +"rem)";
+        document.getElementById("translation").style.width = 22 + "rem";
+        document.getElementById("hakari").style.display = "block";
+        document.getElementById("hakari").src = sorcerer[storedIndex].hakari;
+
+        document.getElementById("button").onclick = function(){
+            document.getElementById("hakari").style.width = 50 + "rem";
+            document.getElementById("hakari").style.height = 50 + "rem";
+            document.getElementById("hakari").style.transform = "translate(-"+ 38 +"rem,-" + 33 +"rem)";
+        setTimeout(() => {
+        document.getElementById("x").style.display = "block";
+            document.getElementById("one").style.display = "block";
+            document.getElementById("two").style.display = "block";
+        }, 320);
+       
+       }
+   
+        document.getElementById("x").onclick = function(){
+            document.getElementById("one").style.textDecoration = "underline";
+            document.getElementById("two").style.textDecoration = "none";
+            document.getElementById("hakari").src = "./assets/love.png";
+            document.getElementById("one").style.display = "none";
+            document.getElementById("two").style.display = "none";
+            document.getElementById("x").style.display = "none";
+            document.getElementById("hakari").style.width = 18 + "rem";
+            document.getElementById("hakari").style.height = 18 + "rem";
+            document.getElementById("hakari").style.transform = "translate(-"+ 7 +"px,-" + 16.5 +"rem)";
+       }
+
+        document.getElementById("one").onclick = function(){
+            document.getElementById("two").style.textDecoration = "none";
+            document.getElementById("one").style.textDecoration = "underline";
+            document.getElementById("hakari").src = "./assets/love.png";
+       }
+
+        document.getElementById("two").onclick = function(){
+            document.getElementById("one").style.textDecoration = "none";
+            document.getElementById("two").style.textDecoration = "underline";
+            document.getElementById("hakari").src = "./assets/game.png";
+        }
+    }
 }
 
 function color(){
