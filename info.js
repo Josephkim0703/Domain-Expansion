d036df8e01b16c104bbba0261a95bd35ce6fa9c5
Joseph <josephkim0703@gmail.com>
2024-01-12 17:13:38 -0800

    done

--- info.js
+++ info.js
@@ -147,12 +147,19 @@ function setup(){
             document.getElementById("hakari").src = "./assets/judgeman.jpg";
         }
     }
-    if(storedIndex == 6 || storedIndex == 7){
+    if(storedIndex == 6){
         document.getElementById("appearance").style.border = 3 +"px solid black";
         document.getElementById("hand").style.border = 3 +"px solid black";
         document.getElementById("domain-vid").style.border = 3 +"px solid black";
         document.getElementById("past").src = sorcerer[storedIndex].former;
         document.getElementById("past").style.display = "block";
+    }
+    if(storedIndex == 7){      
+        document.getElementById("domain-vid").style.visibility = "hidden";
+        document.getElementById("appearance").style.border = 3 +"px solid black";
+        document.getElementById("hand").style.border = 3 +"px solid black";
+        document.getElementById("true").style.display = "block";
+        document.getElementById("true").src = sorcerer[storedIndex].true;
     }
 }
 
